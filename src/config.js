2100f97702a1725e58faae182d2096f14fb9e51b
bandivenkatesh <bandivenky2222@gmail.com>
2025-01-18 01:33:22 +0530

    Update service host URLs in config.js to point to new IP addresses

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-const produtsServiceLocalHost = 'http://localhost:8132/api'
-const usersServiceLocalHost = 'http://localhost:8232/api'
+const produtsServiceLocalHost = 'http://34.74.188.78:8132/api'
+const usersServiceLocalHost = 'http://34.74.188.78:8232/api'
 
-const produtsServiceDevHost = 'http://35.224.78.158:30001/api'
-const usersServiceDevHost = 'http://35.224.78.158:30722/api'
+const produtsServiceDevHost = 'http://34.74.188.78:8132/api'
+const usersServiceDevHost = 'http://34.74.188.78:8232/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
